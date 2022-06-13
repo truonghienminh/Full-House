8fc2f58733fd60b6bf2db034fc0d115cc330ea3c
Trương Hiển Minh <107345914+truonghienminh@users.noreply.github.com>
2022-06-13 07:19:39 +0700

    update code

--- code/01_05/01_05.cpp
+++ code/01_05/01_05.cpp
@@ -1,1 +1,12 @@
-//chưa có code :)
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const double PI = 3.14;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(2) << 2*PI*n << " " << PI*n*n;
+    return 0;
+}
