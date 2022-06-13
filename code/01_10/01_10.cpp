964824a0e3b0c0652256599dabe39daf55f2f4fc
truonghienminh <truonghienminh2006@gmail.com>
2022-06-13 07:24:13 +0700

    fix code

--- code/01_10/01_10.cpp
+++ code/01_10/01_10.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
-    long double n, sum = 0;
+int main(){
+    double n, sum = 0;
     cin >> n;
-    n = abs(n);
-    for (int i = 1; i < n; i++){
-        sum += 1 / (i*(i+1));
+    for (int i = 1; i <= n; i++){
+        sum += 1.0/(i*(i+1));
     }
     cout << fixed << setprecision(2) << sum;
-    return 0;
 }
