52cb7b443906a0315e0a5b7a57e0e870d902e260
truonghienminh <truonghienminh2006@gmail.com>
2022-06-16 21:03:32 +0700

    up code

---
+++ code/05_23/05_23.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, chan[1001], le[1001], sochan = 0, sole = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int x; 
+        cin >> x;
+        if (x % 2 == 0){
+            chan[sochan] = x;
+            sochan++;
+        }
+        else{
+            le[sole] = x;
+            sole++;
+        }
+    }
+    sort(chan, chan + sochan);
+    sort(le, le + sole);
+    for (int i = 0; i < sochan; i++){
+        cout<<chan[i]<<" "; 
+    } 
+    for (int i = 0; i < sole; i++){
+        cout<<le[i]<<" ";
+    } 
+    return 0;
+}
