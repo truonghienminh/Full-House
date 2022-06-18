0b0dbbdb31d168df9a1301b86b6b696b8b502ff9
truonghienminh <truonghienminh2006@gmail.com>
2022-06-18 23:16:08 +0700

    fix code

--- code/03_08 - phần 1/03_08 - phần 1.cpp
+++ code/03_08 - phần 1/03_08 - phần 1.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 bool check1(int n){
     if (n < 2) return false;
-    if (n == 2 || n == 3) return true;
     for (int i = 2; i <= sqrt(n); i++){
         if (n % i == 0)
             return false;
