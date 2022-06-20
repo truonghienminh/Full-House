b09892afb9b5e67ae04404488b71c31a08e7f62f
truonghienminh <truonghienminh2006@gmail.com>
2022-06-20 21:32:44 +0700

    up code

---
+++ code/04_14/04_14.py
@@ -0,0 +1,7 @@
+n = int(input())
+total = 0
+if (n&1) :
+	total = -n/2-1
+else :
+	total = n/2
+print(int(total))
---
+++ code/09_11/09_11.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int a1[n], a2[m];
+    int ans[n+m];
+    for (int i = 0; i < n; i++){
+        cin >> a1[i];
+    }
+    for (int i = 0; i < m; i++){
+        cin >> a2[i];
+    }
+    for (int i = 0; i < m+n;){
+        for (int j = 0; j < n; j++){
+            ans[i] = a1[j];
+            i++;
+        }
+        for (int j = 0; j < m; j++){
+            ans[i] = a2[j];
+            i++;
+        }
+    }
+    sort(ans, ans + n + m);
+    for (int i = 0; i < n+m; i++){
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
