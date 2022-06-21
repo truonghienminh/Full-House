e35deca4eff3c0933f7a552b2a2b82e1b208ebad
truonghienminh <truonghienminh2006@gmail.com>
2022-06-21 20:55:24 +0700

    up code

---
+++ code/03_13 - phần 1/03_13 - phần 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int prime[100001];
+
+void sieve(){
+    for (int i = 1; i <= 100000; i++){
+        prime[i] = i;
+    }
+    for (int i = 2; i <= sqrt(100000); i++){
+        if (prime[i] == i){
+            for (int j = i*i; j <= 100000; j += i){
+                if (prime[j] == j)
+                    prime[j] = i;
+            }
+        }
+    }
+}
+
+int main(){
+    sieve();
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++){
+            cout << prime[i] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
