c67d533c4de3656edd6090bd93eb7ec33dc1a081
truonghienminh <truonghienminh2006@gmail.com>
2022-06-25 23:04:54 +0700

    up code

---
+++ code/03_05 - phần 2/03_05 - phần 2 (cách 2).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int n, count = 0;
+        cin >> n;
+        for (int i = 5; i <= n; i *= 5){
+            count += n / i;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
