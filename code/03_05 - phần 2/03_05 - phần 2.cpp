09df164a44ed047475693075bcfd5cd82106a884
truonghienminh <truonghienminh2006@gmail.com>
2022-06-25 22:49:21 +0700

    up code
    added comment

--- code/03_05 - phần 2/03_05 - phần 2.cpp
+++ code/03_05 - phần 2/03_05 - phần 2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int count(int n){
     int res = 0;
-    for (int i = 5; i <= n; i++){
+    for (int i = 5; i <= n; i++){//đếm số lần xuất hiện của số 5
         int tmp = i;
         while (tmp % 5 == 0){
             res++;
