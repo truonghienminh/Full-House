837a909be506cf210182733be2d59b35b86d9272
truonghienminh <truonghienminh2006@gmail.com>
2022-06-28 19:22:59 +0700

    up code

---
+++ code/05_05/05_05.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m = 0, x;
+    bool check = 1;
+    cin >> n;
+    while (n--){
+        cin >> x;
+       	if (x == 25) 
+       	    m += x;
+        else{
+            if (x - 25 > m){
+                check = 0;
+                break;
+            }
+        }
+    }
+    if (check) cout << "YES";
+    else 
+        cout << "NO";
+    return 0;
+}
