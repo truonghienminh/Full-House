45c712972f35260ada74220f98d61bf2bbbfba70
truonghienminh <truonghienminh2006@gmail.com>
2022-07-11 23:03:33 +0700

    upgrade code

--- code/05_14/05_14.cpp
+++ code/05_14/05_14.cpp
@@ -12,8 +12,6 @@ int main(){
     int arr[n];
     for (int i = 0; i < n; i++){
         cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++){
         check[arr[i]]++;
     }
     for (int i = 0; i < n; i++){
