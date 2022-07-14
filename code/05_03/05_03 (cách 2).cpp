b692cf41c150c285a320cfc3649984a19e3aa88a
truonghienminh <truonghienminh2006@gmail.com>
2022-07-14 16:04:10 +0700

    up code

---
+++ code/05_03/05_03 (cách 2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int &x : arr){
+        cin >> x;
+    }
+    cout << arr[0] << " ";
+    for (int i = 1; i < n; i++){
+        if (arr[i] > arr[i-1])
+            cout << arr[i] << " ";
+    }
+    return 0;
+}
