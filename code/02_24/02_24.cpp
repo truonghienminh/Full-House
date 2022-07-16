16f54c06fd45f9669eba6ba920de01f2fbb219e5
truonghienminh <truonghienminh2006@gmail.com>
2022-07-16 23:20:04 +0700

    up code

---
+++ code/02_24/02_24.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int C(int k, int n) {
+    if (k == 0 || k == n) return 1;
+    if (k == 1) return n;
+    return C(k - 1, n - 1) + C(k, n - 1);
+}
+
+int main(){
+	long long n,m;
+	cin >> n >> m; 
+	long long S = C(m - 1, n - 1); 
+	cout << S; 
+	return 0;
+}
