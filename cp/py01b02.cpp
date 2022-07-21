536d91188528921017361c0a9e197da2384c73e5
truonghienminh <truonghienminh2006@gmail.com>
2022-07-21 15:06:47 +0700

    format code and renamed

--- cp/py01b02.cpp
+++
@@ -1,5 +0,0 @@
-a = input().split()
-x = int(a[0])
-if (x*x*x + 3*x*x + x+1 == 117):
-	print(a)
-print(x*x*x + 3*x*x + x+1)
---
+++ cp/py01b02.py
@@ -0,0 +1,4 @@
+a = input().split()
+x = int(a[0]) if (x * x * x + 3 * x * x + x + 1 == 117): 
+	print(a)
+print(x * x * x + 3 * x * x + x + 1)
