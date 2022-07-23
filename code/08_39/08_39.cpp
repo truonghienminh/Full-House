e0cc2681b4b74f76ce47ba46cf21c783f324f97b
truonghienminh <truonghienminh2006@gmail.com>
2022-07-23 22:41:21 +0700

    upgrade code

--- code/08_39/08_39.cpp
+++ code/08_39/08_39.cpp
@@ -5,27 +5,22 @@ using namespace std;
 int check(string s){
     if (s[0] == '0') return 2;
     for (int i = 0; i < s.length(); i++){
-        if (!(s[i] >= '0' && s[i] <= '9'))
+        if (!isdigit(s[i]))
             return 2;
     }
-    bool check[10];
-    for (int i = 0; i < 10; i++){
-        check[i] = false;
-    }
+    bool check[10] = {0};
     for (int i = 0; i < s.length(); i++){
-        if (s[i] >= '0' && s[i] <= '9')
-            check[s[i]-'0']  = true;
+        if (isdigit(s[i]))
+            check[s[i]-'0'] = true;
     }
     int res = 0;
     for (int i = 0; i < 10; i++){
-        if (check[i] == true)
-            res++;
+        if (check[i] == false)
+            return false;
     }
-    if (res == 10)
-        return 1;
-    else
-        return 0;
+    return true;
 }
+
 int main(){
     int t;
     cin >> t;
