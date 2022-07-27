a05fc9245e9c22932d67c5d30928bb1c922748df
truonghienminh <truonghienminh2006@gmail.com>
2022-07-27 21:43:47 +0700

    format code

--- code/01_28/01_28.cpp
+++ code/01_28/01_28.cpp
@@ -6,23 +6,24 @@ using namespace std;
 int main(){
     int a, b, c;
     cin >> a >> b >> c;
-    double d = b*b - 4*a*c;
-    if(a == 0){
-        if(b == 0 && c == 0) cout << "INF";
-        else if(b == 0 && c!= 0) cout << "NO";
+    double d = b * b - 4 * a * c;
+    if (a == 0){
+        if (b == 0 && c == 0)
+            cout << "INF";
+        else if (b == 0 && c != 0)
+            cout << "NO";
         else
-            cout << fixed << setprecision(2) << -c/b; // bx + c = 0 => x = -c/b;
+            cout << fixed << setprecision(2) << -c / b; // bx + c = 0 => x = -c/b;
     }
     else{
-        if(d > 0 ){
-             cout << fixed << setprecision(2) << (float)(-b + sqrt(d))/(2*a) << " " << (float)(-b - sqrt(d))/(2*a);
-        }
-        else if(d == 0) {
-            cout << fixed << setprecision(2) << (float)(-b)/(2*a);
+        if (d > 0)
+            cout << fixed << setprecision(2) << (float)(-b + sqrt(d)) / (2 * a) << " " << (float)(-b - sqrt(d)) / (2 * a);
+        else if (d == 0){
+            cout << fixed << setprecision(2) << (float)(-b) / (2 * a);
             return 0;
         }
         else{
-            cout<<"NO";
+            cout << "NO";
             return 0;
         }
     }
