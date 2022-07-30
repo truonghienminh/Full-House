2ba574821f17c4d1189c960efab726e3f9b69d97
truonghienminh <truonghienminh2006@gmail.com>
2022-07-30 17:07:19 +0700

    format code

--- code/04_02/04_02.cpp
+++ code/04_02/04_02.cpp
@@ -9,13 +9,15 @@ int64_t n;
 void inp(){
 	cin >> n;
 }
+
 void xuly(){
 	int m = n % 16;
-	if (m < 10){
+	if (m < 10)
 		s.push(m + '0');
-	}else s.push(m + '7');
-
+	else
+		s.push(m + '7');
 }
+
 void out(){
 	while (n > 0){
 		xuly();
@@ -26,8 +28,9 @@ void out(){
 		s.pop();
 	}
 }
+
 int main(){
-    inp();
-    out();
-    return 0;
+	inp();
+	out();
+	return 0;
 }
