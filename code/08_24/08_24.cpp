eff388f33a9592df6ae047474e5970b863bb53f0
truonghienminh <truonghienminh2006@gmail.com>
2022-07-31 21:37:33 +0700

    format code

--- code/08_24/08_24.cpp
+++ code/08_24/08_24.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 #define NMAX 10001
-#define FASTINPUT();    ios::sync_with_stdio(0);\
-                        cin.tie(0);\
-                        cout.tie(0);
+#define FASTINPUT()          \
+    ;                        \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
 typedef long long ll;
 typedef unsigned long long ull;
 string s;
 int temp1 = 0;
 
 void inp(){
-    if (!temp1) {
+    if (!temp1){
         cin.ignore();
         temp1++;
     }
@@ -20,14 +22,16 @@ void inp(){
 }
 
 bool is_num(char x){
-    if (x>=48 && x<= 57) return 1;
+    if (x >= 48 && x <= 57)
+        return 1;
     return 0;
 }
 
 ull stoiii(string n){
     ull temp = 0;
     ull temp1 = 1;
-    for (int i = n.length()-1; i >= 0 ; i--){
+    for (int i = n.length() - 1; i >= 0; i--)
+    {
         temp += (n[i] - '0') * temp1;
         temp1 *= 10;
     }
@@ -42,23 +46,24 @@ void out(){
         string temp;
         if (is_num(s[i])){
             temp += s[i];
-            while (is_num(s[i+j])){
-                temp+=s[i+j];
+            while (is_num(s[i + j])){
+                temp += s[i + j];
                 j++;
             }
             v.push_back(temp);
         }
-        i += j-1;
+        i += j - 1;
     }
-    for (auto x:v) total += stoiii(x);
+    for (auto x : v)
+        total += stoiii(x);
     cout << total << '\n';
 }
 
-int main() {
+int main(){
     FASTINPUT();
     int t;
     cin >> t;
-    while (t--) {
+    while (t--){
         inp();
         out();
     }
