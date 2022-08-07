cae59d61c480414a8c018a0e8eb0eb320094a96e
truonghienminh <truonghienminh2006@gmail.com>
2022-08-07 11:10:08 +0700

    up code

---
+++ code/11_09/11_09.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--){
+        string s;
+        getline(cin, s);
+        set<string> se;
+        stringstream ss(s);
+        string word;
+        vector<string> v;
+        while (ss >> word){
+            v.push_back(word);
+        }
+        for (string x : v){
+            if (se.find(x) != se.end()){
+                cout << x;
+                break;
+            }
+            se.insert(x);
+        }
+        cout << endl;
+    }
+    return 0;
+}
